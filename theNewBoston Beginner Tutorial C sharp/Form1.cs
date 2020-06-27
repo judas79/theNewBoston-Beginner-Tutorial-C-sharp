feba8d8fe14e32525bbafa9c89a65ea25b29d8b3
Rodman Underhill <robotrods@gmail.com>
2020-06-27 18:58:34 -0400

    update tutorial 47 published

--- theNewBoston Beginner Tutorial C sharp/Form1.cs
+++ theNewBoston Beginner Tutorial C sharp/Form1.cs
@@ -24,82 +24,63 @@ namespace theNewBoston_Beginner_Tutorial_C_sharp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            /* L45
-            try
-            {
-                // L45 convert string to integer, using 
-                int iConverted = Convert.ToInt32(textBox1.Text);
-                MessageBox.Show(iConverted.ToString());
-                textBox1.Clear();
-            }
-            catch (Exception)
-            {
-                // message box tells user the error and to clear (the text entered in button1)
-                MessageBox.Show("whole integers only!  \nClear " + textBox1.ToString());                             
-            }
-            */                                                  // GO TO NEXT EXAMPLE
-            /*
-            try
-            {
-                // L45 convert string to integer, using Convert.ToInt32 and adding 100
-                int iConverted = Convert.ToInt32(textBox1.Text)+100;
-                MessageBox.Show(iConverted.ToString());
-                textBox1.Clear();
-            }
-            catch (Exception)
-            {
+            // L47 set object to empty string
+            object oMyString = "TRUE!";
+            object oMyint = 12;
+            
+            // 47 use if and is to determine if var. is a string
+            if(oMyString is string) MessageBox.Show("true, it a string", "Entered as Text: 'true, it a string'");
+            else MessageBox.Show("False");
 
-                MessageBox.Show("whole integers only!  \nClear " + textBox1.ToString());
-            }
-            */                                                   // GO TO NEXT EXAMPLE
-            /*
-            try
-            {
-                // L45 convert string to char, using 
-                char cConverted = Convert.ToChar(textBox1.Text);
-                MessageBox.Show(cConverted.ToString());
-                textBox1.Clear();
-            }
-            catch (Exception)
-            {            
-                MessageBox.Show("One char character only or nothing entered!  \nCLEAR " + textBox1.ToString()+"\n Then start again");
-            }
-            */                                                  // GO TO NEXT EXAMPLE
-            /*
-            try
-            {
-                // L45 convert string to char, using 
-                bool bConverted = Convert.ToBoolean(textBox1.Text);
-                MessageBox.Show(bConverted.ToString());
-                textBox1.Clear();
-            }
-            catch (Exception)
-            {            
-                MessageBox.Show("True or False only!  \nCLEAR " + textBox1.ToString()+"\n Then start again");
-            }
-            */                                                   // GO TO NEXT EXAMPLE
-            /*
-            // NOT IN LESSON get ASCII in Hexadecimal
-            try
-            {
-                
-                // convert string to byte, using 
-                byte[] bConverted = Encoding.Default.GetBytes(textBox1.Text);
-                
-                // get a string with dashes
-                var sConverted = BitConverter.ToString(bConverted);
-                
-                // replace the dash with a space
-                sConverted = sConverted.Replace("-", " ");
+            // 47 use if and is to determine if var. is a string
+            if (oMyString is string) MessageBox.Show(oMyString.ToString(), "string:  oMyString.ToString()");
+            else MessageBox.Show("False");
 
-                MessageBox.Show((textBox1.Text) + "\nconverted to ASCII Hexadecimal\n" + sConverted.ToString());
-                textBox1.Clear();
-            }
-            catch (Exception)
-            {            
-                MessageBox.Show("what did you enter?!  \nCLEAR " + textBox1.ToString()+"\n Then start again");
-            }
+            // 47 use if and is to determine if var. is an object string, if true convert to string
+            if (oMyString is string) MessageBox.Show((string)oMyString, "Convert to string if, if statement is true: (string)oMyString");
+            else MessageBox.Show("False");
+           
+            /* DID NOT WORK
+            // 47 use if and is to determine if var. is a object int, if true convert to string
+            if (oMyint is int)
+                MessageBox.Show((string)oMyint, "Convert to string if, if statement is true: (string)oMyint");
+            else MessageBox.Show("False");
+
+              DID NOT WORK COULDN'T CONVERT OBJECT INT TO INT
+            // 47 use if and is to determine if var. is a object int, if true convert to int
+            if (oMyint is int) MessageBox.Show((int)oMyInt, "Convert to string if, if statement is true: (int)oMyint");
+            else MessageBox.Show("False");
             */
+            
+            // L47 use if/ is to change control variable to cast as a Button
+            Control cMyControl = button1;
+            if(cMyControl is Button)
+            {
+                // notice parenthesis around cast (button)
+                Button bMyButton = (Button)cMyControl;
+
+                // notice .Text not .ToString()
+                MessageBox.Show(bMyButton.Text);
+            }
+
+            // L47 use if/ is to change control variable to cast as a Button using 'as'
+            Control cMyControl2 = button1;
+            if(cMyControl2 is Button)
+            {
+                // notice 'as' after cMyControl2 then Button
+                Button bMyButton = cMyControl2 as Button;
+
+                // notice .Text not .ToString()
+                MessageBox.Show(bMyButton.Text);
+            }
+
+            // 47 use as to convert string object to string
+            object oMyString2 = "TRUE 2!";
+            if(oMyString2 is string)
+            {
+                string sMyString2 = oMyString2 as string;
+                MessageBox.Show(sMyString2);
+            }
         }
 
 
