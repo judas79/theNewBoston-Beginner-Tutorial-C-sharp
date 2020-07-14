7b9ea92d3a52ced3d44d15c9f01a6ac9b6b4f267
Rodman Underhill <robotrods@gmail.com>
2020-07-13 22:56:33 -0400

    update tutorial 72 published

--- theNewBoston Beginner Tutorial C sharp/Form1.Designer.cs
+++ theNewBoston Beginner Tutorial C sharp/Form1.Designer.cs
@@ -63,7 +63,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(120, 203);
+            this.textBox1.Size = new System.Drawing.Size(384, 203);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
