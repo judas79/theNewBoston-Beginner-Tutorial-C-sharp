64caa2dcc9a8f09a88d09abdd0c3275b86e25304
Rodman Underhill <robotrods@gmail.com>
2020-08-09 17:52:56 -0400

    update tutorial 98 published

--- theNewBoston Beginner Tutorial C sharp/Program.cs
+++ theNewBoston Beginner Tutorial C sharp/Program.cs
@@ -12,16 +12,19 @@ namespace theNewBoston_Beginner_Tutorial_C_sharp
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        //static void Main()   // original code
+        static void Main()   // original code
 
         // L97 added a string array to the main to pass to the file being opened
-        static void Main(string[] files)  
+        //static void Main(string[] files)  
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             // L97 for each file opened, create a form1, and pass in s to form1
-            foreach(string s in files)
-            Application.Run(new Form1(s));
+            //foreach(string s in files)
+                //Application.Run(new Form1(s));
+
+            Application.Run(new Form1());
         }
     }
 }
--- theNewBoston Beginner Tutorial C sharp/Properties/Settings.Designer.cs
+++ theNewBoston Beginner Tutorial C sharp/Properties/Settings.Designer.cs
@@ -8,22 +8,53 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace theNewBoston_Beginner_Tutorial_C_sharp.Properties
-{
-
-
+namespace theNewBoston_Beginner_Tutorial_C_sharp.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Name {
+            get {
+                return ((string)(this["Name"]));
+            }
+            set {
+                this["Name"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int Age {
+            get {
+                return ((int)(this["Age"]));
+            }
+            set {
+                this["Age"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public global::System.Windows.Forms.Button buttonA {
+            get {
+                return ((global::System.Windows.Forms.Button)(this["buttonA"]));
+            }
+            set {
+                this["buttonA"] = value;
             }
         }
     }
