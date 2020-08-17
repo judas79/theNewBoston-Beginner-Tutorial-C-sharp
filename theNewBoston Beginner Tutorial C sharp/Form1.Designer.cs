c88fea9b29088b493d85f46f1489886c5ddfff11
Rodman Underhill <robotrods@gmail.com>
2020-08-17 17:32:54 -0400

    update tutorial 107 published

--- theNewBoston Beginner Tutorial C sharp/Form1.Designer.cs
+++ theNewBoston Beginner Tutorial C sharp/Form1.Designer.cs
@@ -29,124 +29,55 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 20);
+            this.button1.Size = new System.Drawing.Size(82, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Go";
+            this.button1.Text = "Send to duck";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(95, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(317, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(-4, 100);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(428, 351);
-            this.webBrowser1.TabIndex = 2;
-            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 39);
+            this.button2.Location = new System.Drawing.Point(273, 13);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(59, 24);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Back";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Click Duck";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // textBox1
             // 
-            this.button3.Location = new System.Drawing.Point(77, 39);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(53, 24);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Refresh";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.textBox1.Location = new System.Drawing.Point(115, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 2;
             // 
-            // button4
+            // webBrowser1
             // 
-            this.button4.Location = new System.Drawing.Point(136, 39);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(55, 24);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Forward";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(357, 39);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(55, 24);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Home";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(12, 70);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(59, 24);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Scrape B6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(77, 70);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 20);
-            this.textBox2.TabIndex = 8;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(351, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Lord%20Jozar";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.webBrowser1.Location = new System.Drawing.Point(12, 42);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(336, 380);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("https://duckduckgo.com/", System.UriKind.Absolute);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(360, 450);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -158,14 +89,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.WebBrowser webBrowser1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
     }
 }
