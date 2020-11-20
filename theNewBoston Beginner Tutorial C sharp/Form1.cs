8ffd771a0bab0177c408c4b2a80817dbc742565e
Rodman Underhill <robotrods@gmail.com>
2020-11-20 17:01:43 -0500

    update tutorial 183 published

--- theNewBoston Beginner Tutorial C sharp/Form1.cs
+++ theNewBoston Beginner Tutorial C sharp/Form1.cs
@@ -8,12 +8,6 @@
 // L179 write #define and #undef, before everything, above
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace theNewBoston_Beginner_Tutorial_C_sharp
@@ -62,6 +56,86 @@ namespace theNewBoston_Beginner_Tutorial_C_sharp
 #if Mark
 //#error Mark is defined  // this will not work at all! or compile until commented out
 #endif //Mark
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string test = "   The Bus    ~";
+            string Scar = "car";
+            int iNum = 22;
+            int iNum2 = 53;
+            int iSum = iNum + iNum2;
+            int iDiv;
+
+            Console.WriteLine(test.StartsWith("T"));
+            Console.WriteLine(test.EndsWith(" Bus"));
+            Console.WriteLine(test.Contains("s"));
+            Console.WriteLine(test.Replace(" ", "-"));
+            Console.WriteLine(test.Trim());
+            Console.WriteLine(test.TrimEnd());
+            Console.WriteLine(test.TrimStart());
+
+            Console.WriteLine($"The {Scar} went {test} yes {iNum}");
+            MessageBox.Show($"The {Scar} went {test} yes {iNum}- int didn't need to be converted ToString()");
+
+            Console.WriteLine($"iNum + iNum2 = {iSum}");
+            Console.Write($"Divide iNum2 / iNum = {iDiv = iNum2 / iNum}:  ");
+            Console.WriteLine("WriteLine to next line, then \\n to skip another line \n");
+            Console.WriteLine($"Divide iNum2 / iNum then to get remainder as decimal iDiv = (iNum2 % iNum)/ iDiv = {iDiv = iNum2 / iNum}.{iDiv = (iNum2 % iNum) / iDiv}");
+            MessageBox.Show($"iNum + iNum2 = {iSum}");
+
+
+            bool bMath = iNum + iNum2 > 10;
+            if (bMath) Console.WriteLine($"iNum + iNum2 = {iSum} which is greater than 10:  {bMath}");
+
+            var vNames = new List<string> { "zero", "one", "two", "three" };  // is the same as
+            //List<string> vNames = new List<string> { "zero", "one", "two", "three" };
+            vNames.Add("four");
+            vNames.Add("6");
+            foreach (var Name in vNames)
+            {
+                Console.WriteLine(Name);
+            }
+
+            var indexName = vNames.IndexOf("four");
+            Console.WriteLine($"index number for 'four' is:  {indexName}");
+
+            vNames.Sort();
+            foreach (var Name in vNames)
+            {
+                Console.WriteLine(Name);
+            }
+            
+            
+            //var vFibonacchi = new List<int> { 1 ,1 };  // is the same as
+            List<int> vFibonacci = new List<int> { 1, 1 };
+            
+            for (int i = 1; i < 19; i++)
+            {
+                var vPrevious = vFibonacci[vFibonacci.Count - 1];
+                var vPrevious2 = vFibonacci[vFibonacci.Count - 2];
+                vFibonacci.Add(vPrevious2 + vPrevious);
+            }
+            int j = 1;
+            foreach (var vItem in vFibonacci)
+            {
+                Console.WriteLine($"{j}. {vItem}");
+                j++;
+            }
+            
+            j = 1;
+            while (vFibonacci.Count < 20)
+            {
+                var vPrevious = vFibonacci[vFibonacci.Count - 1];
+                var vPrevious2 = vFibonacci[vFibonacci.Count - 2];
+                vFibonacci.Add(vPrevious + vPrevious2);
+            }
+            j = 1;
+            foreach (var vItem in vFibonacci)
+            {
+                Console.WriteLine($"{j}. {vItem}");
+                j++;
+            }
         }
     }
 }
